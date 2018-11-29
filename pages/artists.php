d2c2e15f215ce20972692c7307390a557ba3a625
ianrohandavis <43388802+ianrohandavis@users.noreply.github.com>
2018-11-29 09:24:32 -0500

    Add files via upload

---
+++ pages/artists.php
@@ -0,0 +1,69 @@
+<?php 
+
+include("callApi.php");
+
+?>
+
+ 
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+
+
+	<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Music Share</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="playlist.php">Playlist</a></li>
+        
+      </ul>
+      <form method="post" action="header.php" class="navbar-form navbar-left" role="search">
+		  <div class="form-group">
+		    <input type="text" name="search" class="form-control" placeholder="Search">
+		  </div>
+		  <input type="radio" name="artist">Artist 
+		  <input type="radio" name="playlist">Playlist
+		  <input type="radio" name="song">Song
+		  <button type="submit" name="submit" class="btn btn-default">Submit</button>
+		</form>
+      
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+	
+
+<div>
+	<h4>Artist Page</h4>
+	<?php  
+	echo callApi("/api/artists/". $_GET['id'] . "/songs");
+	?>
+</div>
+
+</body>
+</html>
